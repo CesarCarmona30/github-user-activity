8ed6cad7a49a08b75de8f58e0084dde79e3b8505
Cesar Carmona <clcg852@gmail.com>
2025-07-22 21:34:29 -0600

    Update code

--- github_activity.py
+++ github_activity.py
@@ -1,7 +1,14 @@
 import sys
 import requests
 
-username = sys.argv[1]
-url = f'https://api.github.com/users/{username}/events'
-print(requests.get(url).json())
-print(requests.get(url).status_code)
+def get_activity(username): 
+    url = f'https://api.github.com/users/{username}/events'
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        get_activity(sys.argv[1])
+    else:
+        print("Please enter a GitHub username without spaces as an argument")
